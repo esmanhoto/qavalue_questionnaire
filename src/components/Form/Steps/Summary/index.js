4e7435f4e83fda693a5a954af5d17c15543b0bb2
Eduardo Esmanhoto <eduardo_esmanhoto@hotmail.com>
2022-04-09 18:53:06 -0300

    Summary page modified

--- src/components/Form/Steps/Summary/index.js
+++ src/components/Form/Steps/Summary/index.js
@@ -1,6 +1,7 @@
 import { useContext, useEffect } from "react";
 import FormContext from "../../context";
-import { Button, Container, QuestionNumber, QuestionTitle } from "../styles";
+import { Button, Container, QuestionNumber } from "../styles";
+import { SummaryText } from "./styles";
 
 const Summary = () => {
   const { answers, setAnswerHandler, step, setStepHandler } =
@@ -21,11 +22,19 @@ const Summary = () => {
 
   return (
     <Container active={step === 4}>
-      <QuestionNumber>Summary!</QuestionNumber>
-      <QuestionTitle>Congratulations</QuestionTitle>
-      <p>
-        1. {answers?.stepOne} 2.{answers?.stepTwo} 3. {answers?.stepThree}{" "}
-      </p>
+      <QuestionNumber>Summary</QuestionNumber>
+      <SummaryText>
+        <p>
+          Congratulations <span>{answers?.stepOne}!</span>
+        </p>
+        <p>
+          From this quick questionnaire learned you are able to use{" "}
+          <span name="stepOneAnswer">{answers?.stepTwo}</span>. Furthermore, you
+          guessed{" "}
+          <span>{answers?.stepThree === "React" ? "right" : "wrong"}</span>, the
+          framework used in this project was <span>React</span>.
+        </p>
+      </SummaryText>
       <Button onClick={onPrev}>Previous</Button>
       <Button onClick={onRestart}>Start again</Button>
     </Container>
