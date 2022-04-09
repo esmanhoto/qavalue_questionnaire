195b1bec8ee25f4e41f8aea19f9caba121f1abda
Eduardo Esmanhoto <eduardo_esmanhoto@hotmail.com>
2022-04-09 18:50:36 -0300

    qavalue's logo added

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -4,11 +4,13 @@ import StepTwo from "./Steps/StepTwo";
 import StepThree from "./Steps/StepThree";
 import Summary from "./Steps/Summary";
 import { Container } from "./styles";
+import logo from "../../assets/qavalue_logo.png";
 
 const Form = () => {
   return (
     <FormContextProvider>
       <Container>
+        <img src={logo} type="img/png" alt="QAValue's logo" />
         <StepOne />
         <StepTwo />
         <StepThree />
